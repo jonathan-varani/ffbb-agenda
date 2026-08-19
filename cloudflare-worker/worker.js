559f8033f9a5e6f88ebe83441f03aeb5b30b3abd
Jonathan Varani <jonathan.varani@gmail.com>
2026-08-19 11:31:08 +0200

    Config: PAGES_BASE pointe vers basket.varai.fr

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -13,7 +13,7 @@
 const NOCODB_API   = "https://app.nocodb.com";
 const NOCODB_BASE  = "poq54dd1rjvxuki";   // v1 uniquement
 const NOCODB_TABLE = "myrqkg2uylp17q9";   // table ID (utilisé par v1 et v2)
-const PAGES_BASE   = "https://jonathan-varani.github.io/ffbb-agenda";
+const PAGES_BASE   = "https://basket.varai.fr";
 const SENDER_EMAIL = "jonathan.varani@varai.fr";
 const SENDER_NAME  = "Agendas FFBB";
 
